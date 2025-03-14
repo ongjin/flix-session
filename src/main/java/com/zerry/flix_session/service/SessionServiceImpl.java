cbdbe09cfeb760c89610916e1cf77d4d1fce6614
astems01 - kang <kangdh78@astems.co.kr>
2025-03-14 17:41:02 +0900

    docker swarm 적용 쿠버네티스 변경 예정

--- src/main/java/com/zerry/flix_session/service/SessionServiceImpl.java
+++ src/main/java/com/zerry/flix_session/service/SessionServiceImpl.java
@@ -1,16 +1,9 @@
 package com.zerry.flix_session.service;
 
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import com.zerry.flix_session.dto.PatchRequest;
-import com.zerry.flix_session.kafka.SessionEventProducer;
 import com.zerry.flix_session.model.SessionData;
 import com.zerry.flix_session.repository.SessionRepository;
-import com.zerry.flix_session.response.ApiResponse;
 import com.zerry.flix_session.websocket.SessionWebSocketHandler;
 
 import lombok.RequiredArgsConstructor;
@@ -19,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SessionServiceImpl implements SessionService {
     private final SessionRepository sessionRepository;
-    private final SessionEventProducer sessionEventProducer; // Kafka 사용 (옵션)
 
     private final SessionWebSocketHandler webSocketHandler;
 
@@ -28,8 +20,6 @@ public class SessionServiceImpl implements SessionService {
     @Override
     public SessionData createSession(SessionData sessionData) {
         sessionRepository.saveSession(sessionData);
-        // Kafka 이벤트 발행 (옵션)
-        sessionEventProducer.sendSessionEvent("create", sessionData);
         webSocketHandler.broadcast("세션 생성: " + sessionData.getSessionId());
         return sessionData;
     }
@@ -42,8 +32,6 @@ public class SessionServiceImpl implements SessionService {
     @Override
     public SessionData updateSession(SessionData sessionData) {
         sessionRepository.saveSession(sessionData);
-        // Kafka 이벤트 발행 (옵션)
-        sessionEventProducer.sendSessionEvent("update", sessionData);
         webSocketHandler.broadcastSessionUpdate(sessionData.getSessionId(), "세션 업데이트 완료");
         renewSession(sessionData.getSessionId());
         return sessionData;
@@ -53,8 +41,6 @@ public class SessionServiceImpl implements SessionService {
     public void deleteSession(String sessionId) {
         sessionRepository.deleteSession(sessionId);
         webSocketHandler.broadcastSessionUpdate(sessionId, "세션 삭제 완료");
-        // Kafka 이벤트 발행 (옵션)
-        sessionEventProducer.sendSessionEvent("delete", new SessionData(sessionId, null, null, null, 0, null, null));
     }
 
     @Override
